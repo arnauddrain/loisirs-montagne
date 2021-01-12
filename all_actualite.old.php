26c58343285f402c79f6ce835cda96ceb2ac7a36
Arnaud Drain <drain.arnaud@hotmail.fr>
2021-01-12 22:42:59 +0100

    Fix jquery loading

--- all_actualite.old.php
+++ all_actualite.old.php
@@ -58,7 +58,7 @@
  	  ?>
   <script type="text/javascript" src="js/lightbox2-master/dist/js/lightbox-plus-jquery.js"></script>
  <script type="text/javascript" src="js/lightbox2-master/dist/js/lightbox.js"></script>
- <script type="text/javascript" src="js/jquery.js" ></script>
+ <script type="text/javascript" src="node_modules/jquery/dist/jquery.min.js"></script>
  <script type="text/javascript" src="js/masonry.pkgd.min.js"></script>	  
  <script type="text/javascript" src="js/lightbox.js"></script>
  <script type="text/javascript" src="js/script.js" ></script>
